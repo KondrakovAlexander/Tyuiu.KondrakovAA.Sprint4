07ddbd93d8cbbaa68286c3d98da30f3d245fb125
KondrakovAlexander <144276548+KondrakovAlexander@users.noreply.github.com>
2023-11-03 20:28:39 +0500

    Task1

---
+++ Tyuiu.KondrakovAA.Sprint4.Task1.V10.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.KondrakovAA.Sprint4.Task1.V10.Lib;
+
+namespace Tyuiu.KondrakovAA.Sprint4.Task1.V10.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService resp = new DataService();
+            int[] arr = new int[] { 1, 3, 5 };
+            Assert.AreEqual(15, resp.Calculate(arr));
+        }
+    }
+}
---
+++ Tyuiu.KondrakovAA.Sprint4.Task1.V10.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.KondrakovAA.Sprint4.Task1.V10.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.KondrakovAA.Sprint4.Task1.V10.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("af750d15-8251-4c44-89f1-eb475c760736")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
